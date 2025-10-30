02c69b636f4e4861ab7e16e347656f773b123744
Lek <lekm@compute1-client-1.ris.wustl.edu>
2025-10-30 13:51:14 -0500

    Modifications to hail code to run on WashU environment

--- hail_annotate_pipeline.py
+++ hail_annotate_pipeline.py
@@ -1,5 +1,9 @@
 import pprint
 import argparse
+from datetime import datetime
+import os
+import pyspark
+from pyspark.sql import SparkSession
 
 from annotate_frequencies import *
 from generate_split_alleles import *
@@ -145,21 +149,93 @@ def subset_samples_and_variants(mt, subset_path):
 
     return mt
 
+## Hail initiate
+def hail_initialize(jobname: str = None, mem: int = 32):
+    """
+    To initialize the hail environment
+    """
+    today = datetime.now().strftime("%Y-%m-%d")
+    print("Program Starting Date: {}".format(today))
+
+    #HAIL_HOME = subprocess.getoutput("pip3 show hail | grep Location | awk -F' ' '{print $2 \"/hail\"}'")
+    HAIL_HOME = "/usr/local/lib/python3.8/dist-packages/hail"
+    os.environ["HAIL_DIR"] = f"{HAIL_HOME}/backend"
+    # os.environ["HAIL_DIR"] = "/opt/conda/lib/python3.7/site-packages/hail/backend"
+    # os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
+    os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64/jre"
+
+    threads = int(os.environ['LSB_MAX_NUM_PROCESSORS'])
+    print("Run with {} CPUs.".format(threads))
+
+    # Ensure hail-all-spark.jar exists
+    hail_jars = f"{HAIL_HOME}/backend/hail-all-spark.jar"
+    if not os.path.exists(hail_jars):
+        raise FileNotFoundError(f"Cannot find hail-all-spark.jar at {hail_jars}")
+    print(f"Found hail-all-spark.jar at: {hail_jars}")
+
+    # Convert 'mem' to integer if it's not already
+    mem = int(mem)
+
+    # Calculate memory settings
+    spare_memory = 6 #int(mem * 0.25) + 1
+    driver_memory = int(mem * 0.25)  # Use 25% of total memory for driver
+    executor_memory = mem - driver_memory - spare_memory  # Subtract driver memory and overhead for executors
+
+    # Spark Configuration
+    conf = pyspark.SparkConf().setAll([
+        ('spark.master', 'local[{}]'.format(threads)),
+        ('spark.app.name', 'Hail'),
+        ('spark.jars', f'file://{hail_jars}'),
+        ('spark.driver.extraClassPath', f'file://{hail_jars}'),
+        ('spark.executor.extraClassPath', f'file://{hail_jars}'),   #'./hail-all-spark.jar'
+        ('spark.serializer', 'org.apache.spark.serializer.KryoSerializer'),
+        ('spark.kryo.registrator', 'is.hail.kryo.HailKryoRegistrator'),
+        ### https://discuss.hail.is/t/turning-run-combiner-performance-for-hail-local-mode/2318
+        ('spark.driver.memory', '{}g'.format(driver_memory)),
+        ('spark.executor.memory', '{}g'.format(executor_memory)),
+        ('spark.driver.maxResultSize', '{}g'.format(driver_memory)),
+        ### For IOException: No space left on device
+        # https://spark.apache.org/docs/2.3.0/configuration.html
+        ('spark.local.dir', '/storage1/fs1/jin810/Active/BRDGE/tmp'),
+        ### Error: KryoException: Buffer overflow. Available: 0, required: 15069596
+        ('spark.kryoserializer.buffer', '64m'),
+        ('spark.kryoserializer.buffer.max', '1024m'),
+        ('spark.driver.memoryOverhead', '512m'),
+        ('spark.executor.memoryOverhead', '1g'),
+        ### To output some essential info for memory leakage (Garbage collection logging)
+        ('spark.executor.extraJavaOptions', '-XX:+UseParallelGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps ' +
+         '-XX:+PrintGCDateStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M ' +
+         '-XX:ConcGCThreads={}'.format(threads)),
+    ])
+
+    # Initialize Spark Session
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    sc = spark.sparkContext
+
+    # Log file setup
+    #log_base_path = "/storage1/fs1/jin810/Active/testing/yung-chun/Hail_logs"
+    #log_file_name = "hail_vds_new_combiner_{}.log".format(jobname if jobname else today)
+    #logfile = os.path.join(log_base_path, log_file_name)
+    #logfile = create_unique_filename(logfile)
+
+    # Hail Initialization
+    #hl.init(default_reference='GRCh38', sc=sc, log=logfile)
+    hl.init(sc=sc)
+    # hl.init(master='local[16]') -> to use all the CPU
+
+    return spark, sc
 
 def run_pipeline(args):
-    #hl.init(log='./hail_annotation_pipeline.log',master='spark://worker5090.cm.cluster:7077')
-    hl.init(log='./hail_annotation_pipeline.log',master='local[20]',spark_conf={'spark.driver.memory': '8g', 'spark.executor.memory': '8g'})
 
-	#Adding 'chr' prefix so to be compatible
-    rg = hl.get_reference('GRCh37')
-    grch37_contigs = [x for x in rg.contigs if not x.startswith('GL') and not x.startswith('M')]
-    contig_dict = dict(zip(grch37_contigs, ['chr'+x for x in grch37_contigs]))
+    spark, sc = hail_initialize("Lektest", mem="30")
 
     '''
     Import VCF   
     '''
 
-    mt = hl.import_vcf(args.vcf,reference_genome='GRCh38',contig_recoding=contig_dict,array_elements_required=False,force_bgz=True,filter='MONOALLELIC')
+    mt = hl.import_vcf(args.vcf,reference_genome='GRCh38',array_elements_required=False,force_bgz=True)
+    mt = mt.repartition(64)
+
 
     if args.subset:
         mt = subset_samples_and_variants(mt, args.subset)
@@ -175,8 +251,8 @@ def run_pipeline(args):
     meta_ht = hl.import_table(args.meta,delimiter='\t',key='ID')
     ht = annotate_frequencies(mt,meta_ht)
 
-    res_ht = hl.read_table('/mnt/home/mlek/ceph/resources/combined_reference_data_grch38.ht')
-    dbsnp_ht = hl.read_table('/mnt/home/mlek/ceph/resources/dbsnp_b151_grch38_all_20180418.ht')
+    res_ht = hl.read_table('/storage1/fs1/jin810/Active/References/2023_old_references/hail/Ref_HailFormat/combined_reference_data_grch38-2.0.4.ht')
+    dbsnp_ht = hl.read_table('/storage1/fs1/jin810/Active/BRDGE/Reference/dbsnp_b151_grch38_all_20180418.ht')
     #clinvar_ht = hl.read_table('/mnt/home/mlek/ceph/resources/clinvar_20190923.ht')
 
     ht = ht.annotate(rsid = dbsnp_ht[ht.key].rsid,
@@ -210,7 +286,7 @@ def run_pipeline(args):
     )
 
     #VEP Annotate the Hail table (ie. sites-only) using GRCh38 configuration file
-    ht = hl.vep(ht, 'vep-GRCh38.json')
+    ht = hl.vep(ht, 'file:///home/lekm/vep104.json')
 
     ht = prepare_ht_export(ht)
     ht = prepare_ht_for_es(ht)
